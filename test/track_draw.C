13ab85d0c4f8bfc031dcf0b993253f695bb70474
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2007-12-15 21:14:32 +0000

    added more navigation buttons and made display draw command to return a code that represent some action such as advance one event forward or backward, go home, stop etc

--- test/track_draw.C
+++ test/track_draw.C
@@ -7,14 +7,16 @@ class FWDisplayEvent;
 void track_draw() {
   //TFile geomF("cmsGeom20.root");
   
-  TFile f("ttbar.root");
+  TFile f("/data/dmytro/samples/RelVal-RelVal151Higgs-ZZ-4Mu-1183228327-0000-860585E7-5227-DC11-8227-000423D662EE.root");
   fwlite::Event ev(&f);
   
   FWDisplayEvent ed;
-  for(ev.toBegin();
-      !ev.atEnd();
-      ++ev)
-  {
-    ed.draw(ev);
+  for( unsigned int i = 0; i < ev.size(); ) {
+     ev.to(i);
+     int code = ed.draw(ev);
+     if ( code == 1 ) ++i;
+     if ( code == -1 && i>0 ) --i;
+     if ( code == -2 ) i=0;
+     if ( code == -3 ) break;
   }
 }
