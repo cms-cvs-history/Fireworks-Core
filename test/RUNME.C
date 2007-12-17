7a58139c7d830ceb84c5f2f749c20b1301c916bb
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2007-12-17 00:42:23 +0000

    fixed typo

--- test/RUNME.C
+++ test/RUNME.C
@@ -4,10 +4,11 @@
 // #include "Fireworks/Core/interface/FWDisplayEvent.h"
 // however, the minimal 'fix' is to just declare the class it already successful talked with!
 class FWDisplayEvent;
-void runme() {
+void RUNME() {
    //TFile geomF("cmsGeom20.root");
   
-   TFile f("/data/dmytro/samples/RelVal-RelVal151Higgs-ZZ-4Mu-1183228327-0000-860585E7-5227-DC11-8227-000423D662EE.root");
+   // TFile f("/data/dmytro/samples/RelVal-RelVal151Higgs-ZZ-4Mu-1183228327-0000-860585E7-5227-DC11-8227-000423D662EE.root");
+   TFile f("/data/dmytro/samples/RelVal-RelVal151TTbar-1183231184-0000-0CC1FC7F-A127-DC11-88B4-001617C3B6D2.root");
    fwlite::Event ev(&f);
    
    FWDisplayEvent ed;
