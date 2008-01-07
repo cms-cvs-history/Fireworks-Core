c8091509dc8b8dd4f237612c12a46aebb83d231c
Christopher Jones <sha1-2771e8733cab130717cecea538ec39d2e5083d13@cern.ch>
2008-01-07 05:48:46 +0000

    first attempt at View and EventItem infrastructure

--- test/RUNME.C
+++ test/RUNME.C
@@ -8,13 +8,43 @@ void RUNME() {
    //TFile geomF("cmsGeom20.root");
   
    // TFile f("/data/dmytro/samples/RelVal-RelVal151Higgs-ZZ-4Mu-1183228327-0000-860585E7-5227-DC11-8227-000423D662EE.root");
-   TFile f("/data/dmytro/samples/RelVal-RelVal151TTbar-1183231184-0000-0CC1FC7F-A127-DC11-88B4-001617C3B6D2.root");
+  //   TFile f("/data/dmytro/samples/RelVal-RelVal151TTbar-1183231184-0000-0CC1FC7F-A127-DC11-88B4-001617C3B6D2.root");
+   TFile f("ttbar.root");
    fwlite::Event ev(&f);
    
    FWDisplayEvent ed;
+
+   //The following will be moved to a configuration file
+   ed.registerProxyBuilder("ECal","ECalCaloTowerProxy3DLegoBuilder");
+   ed.registerProxyBuilder("HCal","HCalCaloTowerProxy3DLegoBuilder");
    ed.registerProxyBuilder("Tracks","TracksProxy3DBuilder");
    ed.registerProxyBuilder("Muons","MuonsProxy3DBuilder");
-   ed.registerProxyBuilder("Calo","CaloProxyLegoBuilder");
+   //ed.registerProxyBuilder("Calo","CaloProxyLegoBuilder");
+
+   FWEventItem ecal("ECal",
+		    TClass::GetClass("CaloTowerCollection"),
+		    FWDisplayProperties(kBlue),
+		    "towerMaker");
+   ed.registerEventItem(ecal);
+
+   FWEventItem hcal("HCal",
+		    TClass::GetClass("CaloTowerCollection"),
+		    FWDisplayProperties(kRed),
+		    "towerMaker");
+   ed.registerEventItem(hcal);
+
+   FWEventItem tracks("Tracks",
+		      TClass::GetClass("reco::TrackCollection"),
+		      FWDisplayProperties(kGreen),
+		      "ctfWithMaterialTracks");
+   ed.registerEventItem(tracks);
+
+   FWEventItem muons("Muons",
+		     TClass::GetClass("reco::MuonCollection"),
+		     FWDisplayProperties(kRed),
+		     "trackerMuons");
+   ed.registerEventItem(muons);
+   //Finished configuration
    
    for( unsigned int i = 0; i < ev.size(); ) {
       ev.to(i);
