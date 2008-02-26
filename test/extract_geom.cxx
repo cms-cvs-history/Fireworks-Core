156bbac2fe00922d17c27d8628c6ae3d35798df7
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-02-26 02:25:34 +0000

    fixed jets for rho-phi, suppressing low energy jets. Commented out debugging print outs

--- test/extract_geom.cxx
+++ test/extract_geom.cxx
@@ -7,13 +7,39 @@
 
    // display the extract
    TEveManager::Create();
+   /*
+   TEveGeoShapeExtract* container = new TEveGeoShapeExtract( "MuonRhoZ" );
+              container->AddElement( map.getExtract( 588349440 ) );
+              container->AddElement( map.getExtract( 584155136 ) );
+              container->AddElement( map.getExtract( 579960832 ) );
+              container->AddElement( map.getExtract( 575766528 ) );
+   
+              container->AddElement( map.getExtract( 577339392 ) );
+              container->AddElement( map.getExtract( 581533696 ) );
+              container->AddElement( map.getExtract( 585728000 ) );
+              container->AddElement( map.getExtract( 590970880 ) );
+   TFile f("muonz.root", "RECREATE");
+   container->Write("Extract");
+   f.Close();
+   TEveGeoShape::ImportShapeExtract( container, 0);
+    */
    TEveGeoShape::ImportShapeExtract(map.getAllExtracts(),0);
+   /*
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 575766528 ),0 );
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 584155136 ),0 );
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 579960832 ),0 );
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 575766528 ),0 );
    
-   // for ( Int_t i=0; i<10000; ++i) {
-   // TEveGeoShapeExtract* extract = map.getExtract(0x22408000+(i << 18));
-   // if ( extract ) TEveGeoShape::ImportShapeExtract(extract,0);
-   //}
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 577339392 ),0 );
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 581533696 ),0 );
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 585728000 ),0 );
+   TEveGeoShape::ImportShapeExtract( map.getExtract( 590970880 ),0 );
    
+   for ( Int_t i=0; i<1000; ++i) {
+      TEveGeoShapeExtract* extract = map.getExtract(574980096+(i << 18));
+      if ( extract ) TEveGeoShape::ImportShapeExtract(extract,0);
+   }
+   */
    // TEveGeoShape* extract = TEveGeoShape::ImportShapeExtract(map.getExtract(575176704),0);
    
    //   TEveElementList* eveTopElement = new TEveElementList("CMS");
