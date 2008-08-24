267eb9725c7875d8f0c597197f5979f4b774fcae
Dmytro Kovalskyi <sha1-2db2c5875f2923c5009d5b7d11daea50e991b3b2@cern.ch>
2008-08-24 13:25:27 +0000

    working changes for geometry tests

--- test/extract_geom.cxx
+++ test/extract_geom.cxx
@@ -1,5 +1,5 @@
 {
-   gSystem->Load("libFireworksCore");
+   // gSystem->Load("libFireworksCore");
    DetIdToMatrix map;
    const char* geomtryFile = "cmsGeom10.root";
    map.loadGeometry( geomtryFile );
@@ -7,6 +7,26 @@
 
    // display the extract
    TEveManager::Create();
+   
+   for (int i = 637500000; i < 637600000; ++i) {
+      TEveGeoShapeExtract* extract = map.getExtract(i);
+      if ( extract ) {
+	 RPCDetId id(i);
+	 if ( id.region() != 0 ) continue;
+	 std::cout << "id: " << i
+	   << " Region "<<id.region()
+	   << " Ring "<<id.ring()
+	   << " Station "<<id.station()
+	   << " Sector "<<id.sector()
+	   << " Layer "<<id.layer()
+	   << " Subsector "<<id.subsector()
+	   << " Roll "<<id.roll()
+	   << " Tr "<<id.trIndex() << std::endl;
+	 gEve->AddElement(TEveGeoShape::ImportShapeExtract(extract));
+      }
+   }
+
+   
    /*
    TEveGeoShapeExtract* container = new TEveGeoShapeExtract( "MuonRhoZ" );
               container->AddElement( map.getExtract( 588349440 ) );
@@ -23,7 +43,9 @@
    f.Close();
    TEveGeoShape::ImportShapeExtract( container, 0);
     */
-   TEveGeoShape::ImportShapeExtract(map.getAllExtracts(),0);
+   
+   // TEveGeoShape::ImportShapeExtract(map.getAllExtracts(),0);
+   
    /*
    TEveGeoShape::ImportShapeExtract( map.getExtract( 575766528 ),0 );
    TEveGeoShape::ImportShapeExtract( map.getExtract( 584155136 ),0 );
@@ -40,13 +62,15 @@
       if ( extract ) TEveGeoShape::ImportShapeExtract(extract,0);
    }
    */
-   // TEveGeoShape* extract = TEveGeoShape::ImportShapeExtract(map.getExtract(575176704),0);
+   /*
+   TEveGeoShape* extract = TEveGeoShape::ImportShapeExtract(map.getExtract(575176704),0);
    
-   //   TEveElementList* eveTopElement = new TEveElementList("CMS");
-   //   gEve->AddGlobalElement( eveTopElement );
-   // TEveGeoTopNode* eveTopNode = new TEveGeoTopNode(gGeoManager, extract);
-   //    // eveTopNode->UseNodeTrans();
-   //    // gEve->AddGlobalElement(eveTopNode, eveTopNodeElement);
-   // gEve->AddGlobalElement(eveTopElement);
+   TEveElementList* eveTopElement = new TEveElementList("CMS");
+   gEve->AddGlobalElement( eveTopElement );
+   TEveGeoTopNode* eveTopNode = new TEveGeoTopNode(gGeoManager, extract);
+      // eveTopNode->UseNodeTrans();
+      // gEve->AddGlobalElement(eveTopNode, eveTopNodeElement);
+   gEve->AddGlobalElement(eveTopElement);
+    */
 }
 
