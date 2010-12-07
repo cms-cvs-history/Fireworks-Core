eddb0c8cbbc901826ab5ef108c33e333b921bca9
Thomas Mc Cauley <sha1-9b8b7cc53491404f537ff22c65441882cbee992a@cern.ch>
2010-12-07 10:30:55 +0000

    * update to changes on HEAD * add geometry table to "windows" menu * add necessary code for skeleton geometry table view

--- interface/FWDigitSetProxyBuilder.h
+++ interface/FWDigitSetProxyBuilder.h
@@ -16,7 +16,7 @@
 //
 // Original Author:  Alja Mrak-Tadel
 //         Created:  Tue Oct 19 12:00:57 CEST 2010
-// $Id: FWDigitSetProxyBuilder.h,v 1.1 2010/10/20 19:26:58 amraktad Exp $
+// $Id: FWDigitSetProxyBuilder.h,v 1.3 2010/12/03 20:38:57 amraktad Exp $
 //
 
 // system include files
@@ -45,8 +45,21 @@ public:
    // ---------- member functions ---------------------------
 
 protected:
+
+   // AMT: temproary structure since TEveBoxSet::BFreeBox_t is protected
+   // this workaround should be  removed in next root patch
+   struct BFreeBox_t
+   {
+      Int_t  fValue;  
+      void  *fUserData;
+      Float_t fVertices[8][3]; 
+      BFreeBox_t(Int_t v=0) : fValue(v), fUserData(0) {}
+   };
+
+
    TEveBoxSet* addBoxSetToProduct(TEveElementList* product);
    void addBox(TEveBoxSet* set, const float* pnts, const FWDisplayProperties& dp);
+   TEveBoxSet* getBoxSet() const { return m_boxSet; } 
 
 private:
    FWDigitSetProxyBuilder(const FWDigitSetProxyBuilder&); // stop default
@@ -60,6 +73,8 @@ private:
    static TString getTooltip(TEveDigitSet* set, int idx);
 
    TEveDigitSet* digitSet(TEveElement* product);
+
+   TEveBoxSet* m_boxSet;	
 };
 
 
